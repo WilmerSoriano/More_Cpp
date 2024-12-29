ee77c071519259a3a43764dbb13e438ed86a28fc
WilmerSoriano <Official_Wilmer.2020@outlook.com>
2024-12-29 16:35:38 -0600

    still working on code, comment for self reminder

---
+++ Thread_Workers/Workers.cpp
@@ -0,0 +1,101 @@
+/*
+  Code is under debugging, comments are here to help
+  get an idea of what the project is about
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <cmath>
+using namespace std;
+
+class Work { // Work to do – calc using theta and store result
+  public:
+    Work(double theta) : _theta{theta} { }
+    double theta() {return _theta;}
+    void set_result(double result) {_result = result;}
+    double result() {return _result;}
+  private:
+    double _theta;
+    double _result;
+};
+
+// a. Declare a global vector of Work objects named “work”
+        std::vector<Work> work;
+
+
+
+// b. Declare an iterator for “work” named “next_work”
+//     (Remember, class "iterator" is nested within the container)
+//     It will be initialized in main, so don’t initialize here
+
+
+
+// c. Declare a mutual exclusion object named “m”, which will
+//     protect “next_work” during updates by the threads
+
+
+//
+// The code each thread will execute
+//
+
+void worker() {
+  bool done = false;
+  while (true) {    // Loop until all work is assigned a thread
+
+// d. Lock mutual exclusion object “m” to block other threads
+//     from this region while this thread executes it
+
+
+// e. Use next_work’s copy constructor to create an iterator
+//       named “i” for the work vector 
+
+
+
+    // Update next_work to point to the next item, if any
+    if (next_work != work.end()) next_work++; else done = true;
+
+// f. Unlock mutual exclusion object “m”
+
+
+// If no more work left, exit the thread
+    if (done) break; 
+    i->set_result(sin(i->theta()*M_PI/180));                  // The calculation
+    this_thread::sleep_for(chrono::milliseconds(rand()%3+1)); // Pause
+  }
+}
+
+
+//
+// Main
+//
+
+int main() {
+  // Create the work to be done by the threads
+  for (double theta = 0.0; theta < 360.0; theta += 0.1)
+    work.push_back(Work{theta});
+
+  // g. Initialize the “next_work” iterator to point to
+  //     the first element of vector “work”
+
+
+
+
+  // h. Create and run 10 threads, each running “worker”,
+  //     and storing them in an array or vector named “threads”
+
+
+
+
+  // i. Join the 10 threads as they complete
+  
+
+
+  // Print out the results from the thread pool
+  for (Work& w : work)
+    cout << "sin(" << w.theta() << ") = " 
+                   << w.result() << endl;
+}
